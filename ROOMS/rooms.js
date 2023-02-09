7283bc3ff0dcf4a314a3844537a9441b9f0fdd5e
Lisus Lev <lisus.lev@yandex.ru>
2023-02-09 13:30:10 +0200

    1

--- ROOMS/rooms.js
+++ ROOMS/rooms.js
@@ -8,7 +8,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const allCreatedRooms = [];
-const Room = require('./models');
+const Room = require('./roommodels');
+const User = require('../users/usermodels');
 
 app.post('/api/rooms', (req, res) => {
     const addNewRoom = req.body.roomNumber
@@ -56,21 +57,59 @@ app.get('/api/rooms/:id', (req, res) => {
     res.json(findRoom ? findRoom : 'not found');
 });
 
-// app.post('/api/rooms/:id/enteruser', (req, res) => {
-//     let id = req.params.id
-//     let user = req.body.user
-//     if ((allCreatedRooms[id]) && (!(roomStatus[id]))) {
-//         roomStatus[id] = user.id
-//     }
-// });
+const user = new User(2, 'John', 'Doe', 'P1234567', '01/01/2000');
 
-// app.post('/api/rooms/:id/exituser', (req, res) => {
-//     let id = req.params.id
-//     let user = req.body.user
-//     if ((allCreatedRooms[id]) && (roomStatus[id] == user)) {
-//         roomStatus[id] = 0
-//     }
-// });
+app.post('/api/rooms/:id/enteruser', (req, res) => {
+    let id = req.params.id
+    let findRoom = null;
+    for (const room of allCreatedRooms) {
+        if (room.roomNumber == id) {
+            findRoom = room;
+            break;
+        }
+    }
+    if (findRoom) {
+        if (findRoom.roomStatus == 0) {
+            findRoom.roomStatus = user.id
+            console.log(findRoom);
+            res.json(findRoom);
+        }
+        else {
+            console.log('room is occupied by user: ', user.id);
+            res.json('room is occupied by user: ' + user.id);
+        }
+    }
+    else {
+        console.log('room not found');
+        res.json('room not found');
+    }
+});
+
+app.post('/api/rooms/:id/exituser', (req, res) => {
+    let id = req.params.id
+    let findRoom = null;
+    for (const room of allCreatedRooms) {
+        if (room.roomNumber == id) {
+            findRoom = room;
+            break;
+        }
+    }
+    if (findRoom) {
+        if (findRoom.roomStatus != 0) {
+            findRoom.roomStatus = 0
+            console.log(findRoom);
+            res.json(findRoom);
+        }
+        else {
+            console.log('room is not occupied');
+            res.json('room is not occupied');
+        }
+    }
+    else {
+        console.log('room not found');
+        res.json('room not found');
+    }
+});
 
 app.listen(5050, () => {
     console.log('server started');
---
+++ USERS/usermodels.js
@@ -0,0 +1,11 @@
+class User {
+    constructor(id, first_name, last_name, pasport_id, data_birth) {
+        this.id = id
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.pasport_id = pasport_id;
+        this.data_birth = data_birth;
+    }
+  }
+  
+module.exports = User;
